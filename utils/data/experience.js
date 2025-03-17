926b682b9d9682cafd16782a39de34bc49f9f7e8
seydou <dousey2003@gmail.com>
2025-03-17 14:19:06 +0000

    update experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,21 +1,22 @@
 export const experiences = [
+  
   {
-    id: 1,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Mar 2024 - Present)"
+    id: 3,
+    title: "IT Developer",
+    company: "National University Hospital Center of Fann (CHNU Fann)",
+    duration: "Apr 2024 - Dec 2024"
   },
   {
     id: 2,
     title: 'Backend Developer',
-    company: "Freelance",
-    duration: "(Dec 2023 - Feb 2024)"
+    company: "Guediawaye Computer Club (CIG)",
+    duration: "Mar 2024 "
   },
   {
-    id: 3,
-    title: "Web Developer",
-    company: "Simplon Senegal (freelance)",
-    duration: "(Jul 2023 - Mar 2024)"
+    id: 1,
+    title: "Backend Developer",
+    company: "Simplon Senegal",
+    duration: "Jul 2023 - Feb 2024"
   },
   
 ]
